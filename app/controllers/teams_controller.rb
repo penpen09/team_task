198c0bc9a4e3063680fcdc5d96d657700b33063c
penpen09 <ar-el.mbl.10@outlook.jp>
2020-09-17 11:27:30 +0900

    update:team/controllerでリーダー以外は編集できないように設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,10 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user == @team.owner
+      redirect_to @team , notice: I18n.t('views.messages.not_edit_team')
+  end
 
   def create
     @team = Team.new(team_params)
